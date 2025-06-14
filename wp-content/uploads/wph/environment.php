2a986a94fcc0c9fdfdf4fe006a7b57a1944c1506
Abdallh ELzayat <abdallhelzayat900@gmail.com>
2025-06-14 11:49:55 +0300

    some changes

---
+++ wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"blocksy","mapped_name":""},"allowed_paths":["C:\/laragon\/www\/abdallh\/wp-content\/themes"],"cache_path":"C:\/laragon\/www\/abdallh\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/"}'; ?>
